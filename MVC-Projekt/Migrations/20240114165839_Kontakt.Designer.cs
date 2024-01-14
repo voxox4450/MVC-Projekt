a5a94ec3bfffb749300402bfed30efcc3506df63
voxox4450 <voxoxgaming@gmail.com>
2024-01-14 18:10:21 +0100

    e

--- MVC-Projekt/Migrations/20240114165839_Kontakt.Designer.cs
+++ MVC-Projekt/Migrations/20240114165839_Kontakt.Designer.cs
@@ -4,6 +4,7 @@ using MVC_Projekt.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace MVC_Projekt.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240114165839_Kontakt")]
+    partial class Kontakt
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ MVC-Projekt/Migrations/20240114165839_Kontakt.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MVC_Projekt.Migrations
+{
+    /// <inheritdoc />
+    public partial class Kontakt : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Adresy_Kontakty_KontaktId",
+                table: "Adresy");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Adresy_KontaktId",
+                table: "Adresy");
+
+            migrationBuilder.AddColumn<int>(
+                name: "AdresId",
+                table: "Kontakty",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Kontakty_AdresId",
+                table: "Kontakty",
+                column: "AdresId",
+                unique: true,
+                filter: "[AdresId] IS NOT NULL");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Kontakty_Adresy_AdresId",
+                table: "Kontakty",
+                column: "AdresId",
+                principalTable: "Adresy",
+                principalColumn: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Kontakty_Adresy_AdresId",
+                table: "Kontakty");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Kontakty_AdresId",
+                table: "Kontakty");
+
+            migrationBuilder.DropColumn(
+                name: "AdresId",
+                table: "Kontakty");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Adresy_KontaktId",
+                table: "Adresy",
+                column: "KontaktId",
+                unique: true,
+                filter: "[KontaktId] IS NOT NULL");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Adresy_Kontakty_KontaktId",
+                table: "Adresy",
+                column: "KontaktId",
+                principalTable: "Kontakty",
+                principalColumn: "Id");
+        }
+    }
+}
