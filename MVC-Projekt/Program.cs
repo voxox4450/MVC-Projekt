feab1f4ca3cd982aa23307ec5cd463773a434e80
Jakub Malik <malik.j@poczta.fm>
2024-01-14 13:24:26 +0100

    Szkielet usera

--- MVC-Projekt/Program.cs
+++ MVC-Projekt/Program.cs
@@ -1,4 +1,13 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using MVC_Projekt.Areas.Identity.Data;
+using MVC_Projekt.Data;
 var builder = WebApplication.CreateBuilder(args);
+var connectionString = builder.Configuration.GetConnectionString("ApplicationDbContextConnection") ?? throw new InvalidOperationException("Connection string 'ApplicationDbContextConnection' not found.");
+
+builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(connectionString));
+
+builder.Services.AddDefaultIdentity<ApplicationUser>(options => options.SignIn.RequireConfirmedAccount = true).AddEntityFrameworkStores<ApplicationDbContext>();
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
