947c4b5f00984ba4ed8a927871c30b9bf7cb7999
Jakub Malik <malik.j@poczta.fm>
2024-01-14 13:15:51 +0100

    s

---
+++ MVC-Projekt/Controllers/ContactsController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MVC_Projekt.Models;
+
+namespace MVC_Projekt.Controllers
+{
+    public class ContactsController : Controller
+    {
+        // Akcja wyświetlająca formularz dodawania kontaktu
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // Akcja przetwarzająca dane z formularza dodawania kontaktu
+        [HttpPost]
+        public ActionResult Create(Contact contact)
+        {
+            // Tutaj możesz dodać kod obsługi dodawania kontaktu do bazy danych
+            // np. używając Entity Framework
+            // DbContext.Add(contact);
+            // DbContext.SaveChanges();
+
+            // Przekierowanie po dodaniu kontaktu
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
